65652912db2b5fc75286a61e874c2fa3b6e9e31e
marianotbh <mariano.tbh@gmail.com>
2020-02-17 02:51:18 -0300

    add basic routes for the new controllers

--- index.php
+++ index.php
@@ -8,9 +8,16 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+/* Controllers */
 use App\Controllers\AuthController;
+use App\Controllers\MenuController;
+use App\Controllers\OrderController;
+use App\Controllers\ReviewController;
 use App\Controllers\RoleController;
+use App\Controllers\TableController;
 use App\Controllers\UserController;
+
+/* Middleware */
 use App\Middleware\CorsMiddleware;
 use App\Middleware\ErrorHandlerMiddleware;
 use App\Middleware\AuthMiddleware;
@@ -44,6 +51,38 @@ $app->group('/roles', function (App $app) {
     $app->post('[/]', RoleController::class . ":create");
     $app->put('/{id}[/]', RoleController::class . ":update");
     $app->delete('/{id}[/]', RoleController::class . ":delete");
+});
+
+$app->group('/orders', function (App $app) {
+    $app->get('[/]', OrderController::class . ":list");
+    $app->get('/{id}[/]', OrderController::class . ":read");
+    $app->post('[/]', OrderController::class . ":create");
+    $app->put('/{id}[/]', OrderController::class . ":update");
+    $app->delete('/{id}[/]', OrderController::class . ":delete");
+});
+
+$app->group('/tables', function (App $app) {
+    $app->get('[/]', TableController::class . ":list");
+    $app->get('/{id}[/]', TableController::class . ":read");
+    $app->post('[/]', TableController::class . ":create");
+    $app->put('/{id}[/]', TableController::class . ":update");
+    $app->delete('/{id}[/]', TableController::class . ":delete");
+});
+
+$app->group('/menu', function (App $app) {
+    $app->get('[/]', MenuController::class . ":list");
+    $app->get('/{id}[/]', MenuController::class . ":read");
+    $app->post('[/]', MenuController::class . ":create");
+    $app->put('/{id}[/]', MenuController::class . ":update");
+    $app->delete('/{id}[/]', MenuController::class . ":delete");
+});
+
+$app->group('/reviews', function (App $app) {
+    $app->get('[/]', ReviewController::class . ":list");
+    $app->get('/{id}[/]', ReviewController::class . ":read");
+    $app->post('[/]', ReviewController::class . ":create");
+    $app->put('/{id}[/]', ReviewController::class . ":update");
+    $app->delete('/{id}[/]', ReviewController::class . ":delete");
 });
 
 $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function (Request $req, Response $res) {
